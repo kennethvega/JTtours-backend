477d8df89d67976913bb92415f6dac71e1850bf4
kenneth vega <kenneth.trinidad.vega@gmail.com>
2023-01-02 13:57:52 +0800

    show stack

--- middleware/errorMiddleware.ts
+++ middleware/errorMiddleware.ts
@@ -5,7 +5,7 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   res.status(statusCode);
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "development" ? err.stack : null, // --> show stack only in development
+    stack: process.env.NODE_ENV === "production" ? err.stack : null, // --> show stack only in development
   });
 };
 
